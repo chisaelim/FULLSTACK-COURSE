48b365d456864b1329a6db035451d42991bd54e8
chisaelim <chisae0123@gmail.com>
2026-07-14 22:26:32 +0700

    session6.4

--- vuejs-app/src/components/includes/LeftSidebar.vue
+++ vuejs-app/src/components/includes/LeftSidebar.vue
@@ -14,7 +14,6 @@
           <router-link :to="{ name: 'profile' }" class="d-block">{{ userStore.name }}</router-link>
         </div>
       </div>
-
       <nav class="mt-2">
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           <li class="nav-item">
@@ -44,6 +43,22 @@
           </li>
         </ul>
       </nav>
+
+      <!-- SidebarSearch Form -->
+      <div class="form-inline">
+        <div class="input-group">
+          <input class="form-control form-control-sidebar" type="text" placeholder="Search" aria-label="Search">
+          <div class="input-group-append">
+            <button class="btn btn-sidebar">
+              <i class="fas fa-search fa-fw"></i>
+            </button>
+          </div>
+        </div>
+      </div>
+      <nav class="mt-2">
+        <ChatList :chats="chats"></ChatList>
+        <UserList :users="users"></UserList>
+      </nav>
     </div>
   </aside>
 </template>
@@ -51,6 +66,24 @@
 import emptyImage from '@/assets/images/emptyImage.png';
 import logoImage from '@/assets/images/logoImage.webp';
 import { useUserStore } from '@/stores/user';
-
+import { ref, onMounted } from 'vue';
+import { apiGetChats, apiGetChatUsers } from '@/functions/api/chat';
+import ChatList from '@/components/includes/controls/ChatList.vue';
+import UserList from '@/components/includes/controls/UserList.vue';
 const userStore = useUserStore();
+const chats = ref([]);
+const users = ref([]);
+
+onMounted(() => {
+  generateChats();
+  generateUsers();
+});
+async function generateChats() {
+  const response = await apiGetChats();
+  chats.value = response.data.chats;
+}
+async function generateUsers() {
+  const response = await apiGetChatUsers();
+  users.value = response.data.users;
+}
 </script>
