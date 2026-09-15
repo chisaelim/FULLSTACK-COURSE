a28b89ff00ae15a151253756619189c6bc6bd51c
chisaelim <chisae0123@gmail.com>
2026-09-15 21:25:07 +0700

    session4.4

--- vuejs-app/src/components/includes/LeftSidebar.vue
+++ vuejs-app/src/components/includes/LeftSidebar.vue
@@ -8,37 +8,10 @@
     <div class="sidebar">
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img :src="emptyImage" class="img-circle elevation-2" alt="User Image">
+          <img :src="userStore.profile_thumbnail || emptyImage" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
           <router-link :to="{ name: 'profile' }" class="d-block">{{ userStore.name }}</router-link>
-        </div>
-      </div>
-
-      <!-- SidebarSearch Form -->
-      <div class="form-inline">
-        <div class="input-group" data-widget="sidebar-search">
-          <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
-          <div class="input-group-append">
-            <button class="btn btn-sidebar">
-              <i class="fas fa-search fa-fw"></i>
-            </button>
-          </div>
-        </div>
-        <div class="sidebar-search-results">
-          <div class="list-group"><a href="#" class="list-group-item">
-              <div class="search-title"><strong class="text-light"></strong>N<strong
-                  class="text-light"></strong>o<strong class="text-light"></strong> <strong
-                  class="text-light"></strong>e<strong class="text-light"></strong>l<strong
-                  class="text-light"></strong>e<strong class="text-light"></strong>m<strong
-                  class="text-light"></strong>e<strong class="text-light"></strong>n<strong
-                  class="text-light"></strong>t<strong class="text-light"></strong> <strong
-                  class="text-light"></strong>f<strong class="text-light"></strong>o<strong
-                  class="text-light"></strong>u<strong class="text-light"></strong>n<strong
-                  class="text-light"></strong>d<strong class="text-light"></strong>!<strong class="text-light"></strong>
-              </div>
-              <div class="search-path"></div>
-            </a></div>
         </div>
       </div>
 
